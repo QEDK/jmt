23d4658115a4e9bfad5e818b5b6162ef4bf70097
qedk <1994constant@gmail.com>
2024-06-17 14:05:18 +0530

    fix: fix impl for inclusion proof

--- src/JellyfishMerkleTreeVerifier.sol
+++ src/JellyfishMerkleTreeVerifier.sol
@@ -12,7 +12,7 @@ library JellyfishMerkleTreeVerifier {
         bytes32[] siblings;
     }
 
-    function verifyProof(bytes32 root, Leaf calldata leaf, Proof calldata proof) public pure returns (bool) {
+    function verifyProof(bytes32 root, Leaf calldata leaf, Proof calldata proof) external pure returns (bool) {
         if (leaf.addr != 0x0 && leaf.valueHash != 0x0) { // Node existence expects inclusion proof
             if (proof.leaf.addr != 0x0 && proof.leaf.valueHash != 0x0) {
                 // Prove inclusion with inclusion proof
@@ -24,7 +24,7 @@ library JellyfishMerkleTreeVerifier {
                 return false;
             }
         }
-        else { // Node absense expects exclusion proof
+        else { // Node absence expects exclusion proof
             if (proof.leaf.addr != 0x0 && proof.leaf.valueHash != 0x0) { // The inclusion proof of another node
                 if (leaf.addr == proof.leaf.addr || commonLengthPrefixInBits(leaf.addr, proof.leaf.addr) < proof.siblings.length) {
                     return false;
@@ -35,20 +35,23 @@ library JellyfishMerkleTreeVerifier {
         if (proof.leaf.addr == 0x0 && proof.leaf.valueHash == 0x0) {
             calculatedRoot = 0x0;
         } else {
-            calculatedRoot = sha256(abi.encodePacked(proof.leaf.addr,proof.leaf.valueHash));
+            calculatedRoot = sha256(abi.encodePacked(hex'4a4d543a3a4c6561664e6f6465', proof.leaf.addr, proof.leaf.valueHash));
         }
-        for (uint256 i = 256 - proof.siblings.length - 1; i >= 0; i -= 1) {
-            if (getIthBit(calculatedRoot, i) == 1) {
-                calculatedRoot = sha256(abi.encodePacked(proof.siblings[i], calculatedRoot));
+        for (uint256 i = 0; i < proof.siblings.length;) {
+            if (getIthBit(leaf.addr, proof.siblings.length - i) == 1) {
+                calculatedRoot = sha256(abi.encodePacked(hex'4a4d543a3a496e74726e616c4e6f6465', proof.siblings[i], calculatedRoot));
             } else {
-                calculatedRoot = sha256(abi.encodePacked(calculatedRoot, proof.siblings[i]));
+                calculatedRoot = sha256(abi.encodePacked(hex'4a4d543a3a496e74726e616c4e6f6465', calculatedRoot, proof.siblings[i]));
+            }
+            unchecked {
+                ++i;
             }
         }
         return calculatedRoot == root;
     }
 
-    function commonLengthPrefixInBits(bytes32 a, bytes32 b) internal pure returns (uint256) {
-        uint256 xor = uint(a) ^ uint(b);
+    function commonLengthPrefixInBits(bytes32 a, bytes32 b) private pure returns (uint256) {
+        uint256 xor = uint256(a) ^ uint256(b);
         uint256 leadingZeros = 0;
         while (xor > 0) {
             xor >>= 1;
@@ -57,7 +60,7 @@ library JellyfishMerkleTreeVerifier {
         return 256 - leadingZeros;
     }
 
-    function getIthBit(bytes32 value, uint256 i) internal pure returns (uint256) {
+    function getIthBit(bytes32 value, uint256 i) private pure returns (uint256) {
         return (uint256(value) >> i) & 1;
     }
 }
--- test/JellyfishMerkleTreeVerifierTest.sol
+++ test/JellyfishMerkleTreeVerifierTest.sol
@@ -5,13 +5,40 @@ import "src/JellyfishMerkleTreeVerifier.sol";
 import "forge-std/Test.sol";
 
 contract JellyfishMerkleTreeVerifierTest is Test {
+    JellyfishMerkleTreeVerifierUser jmtUser;
 
+    function setUp() external {
+        jmtUser = new JellyfishMerkleTreeVerifierUser();
+    }
+
+    function test_verifyProof() public view {
+        JellyfishMerkleTreeVerifier.Leaf memory leaf = JellyfishMerkleTreeVerifier.Leaf({
+            addr: 0x0101010101010101010101010101010101010101010101010101010101010101,
+            valueHash: 0xa665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3
+        });
+        bytes32[] memory siblings = new bytes32[](7);
+        siblings[0] = 0xd7f314b6ee65a14ca2805610debeac9f93d03386fb08e14f95beb1fb4bef03ac;
+        siblings[1] = 0x0000000000000000000000000000000000000000000000000000000000000000;
+        siblings[2] = 0x9c1e1bce27a649373c74d44e7e97c1d269b586631d0d31ef63813447d721fb58;
+        siblings[3] = 0x0000000000000000000000000000000000000000000000000000000000000000;
+        siblings[4] = 0x0000000000000000000000000000000000000000000000000000000000000000;
+        siblings[5] = 0x0000000000000000000000000000000000000000000000000000000000000000;
+        siblings[6] = 0x0000000000000000000000000000000000000000000000000000000000000000;
+        JellyfishMerkleTreeVerifier.Proof memory proof = JellyfishMerkleTreeVerifier.Proof({
+            leaf: JellyfishMerkleTreeVerifier.Leaf({
+                addr: 0x0101010101010101010101010101010101010101010101010101010101010101,
+                valueHash: 0xa665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3
+            }),
+            siblings: siblings
+        });
+        assertTrue(jmtUser.verifyProof(0x7be2a40f7a47936cf2b398ebc5cabbfc8019ecb458be5dd8a5473b2dad1fbe2e, leaf, proof));
+    }
 }
 
 contract JellyfishMerkleTreeVerifierUser {
     using JellyfishMerkleTreeVerifier for bytes32;
 
-    function verifyProof(bytes32 root, JellyfishMerkleTreeVerifier.Leaf calldata leaf, JellyfishMerkleTreeVerifier.Proof calldata proof) public pure returns (bool) {
+    function verifyProof(bytes32 root, JellyfishMerkleTreeVerifier.Leaf calldata leaf, JellyfishMerkleTreeVerifier.Proof calldata proof) public view returns (bool) {
         return root.verifyProof(leaf, proof);
     }
 }
